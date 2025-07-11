cce60893e3a4f82f80243af3c22f268f801ac2bc
Lee Jihun <ljh09060@gmail.com>
2025-07-11 23:43:32 +0900

    feat: Add new solution

---
+++ baekjoon/14581/14581.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    char name[100];
+    scanf("%s", name);
+    
+    printf(":fan::fan::fan:\n:fan::%s::fan:\n:fan::fan::fan:", name);
+    
+    return 0;
+}
