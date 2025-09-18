3bbfdb40cd9fbef5bde860f6bf5acc9daab47d39
Lee Jihun <ljh09060@gmail.com>
2025-09-19 08:47:19 +0900

    feat: Add new solution

---
+++ baekjoon/4134/4134.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool f(int x){
+    int div = 
+    while(x > 1){
+        
+    }
+    return true;
+}
+
+int main(void) {
+    int t = 0;
+    cin >> t;
+
+    for(int i = 0; i < t; i++){
+        int n;
+        cin >> n;
+
+        while(true){
+            int x = n;
+            if(f(x)) break;
+            x++;
+        }
+    }
+
+    return 0;
+}
