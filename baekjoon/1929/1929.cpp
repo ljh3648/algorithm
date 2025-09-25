22471e5c485d7df32b25f5c26d9fee72c4241832
Lee Jihun <ljh09060@gmail.com>
2025-09-26 08:48:37 +0900

    feat: 2트째

--- baekjoon/1929/1929.cpp
+++ baekjoon/1929/1929.cpp
@@ -5,21 +5,31 @@ int main(void) {
     int m, n;
     cin >> m >> n;
 
-    if (m == 1) m = 2;
+    // 체는 소수가 아닌것들을 미리 판단할 수 있음.
+
+    bool isPrime[n - m + 1] = { false };
 
     for (int i = m; i <= n; i++) {
+        if (isPrime[i - m]) continue;
 
         bool flag = true;
         for (int j = 2; j * j <= i; j++) {
-            if(i % j == 0) {
+            if (i % j == 0) {
                 flag = false;
                 break;
             }
         }
 
         if (flag) {
-            cout << i << endl;
+            for (int k = i; k <= n; k *= i) {
+                isPrime[k - m] = false; // 소수가 아님
+            }
         }
+    }
+
+    for (int i = 0; i <= n - m; i++) {
+        if(!isPrime[i]) continue;
+        cout << isPrime[i] << "\n";
     }
 
     return 0;
